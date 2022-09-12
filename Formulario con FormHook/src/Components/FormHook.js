e85a49943f96af2efe924b72fdd4375cf77082ba
BorisLeightonI <borisleighton@gmail.com>
2022-09-12 12:48:10 -0500

    fixed

--- Formulario con FormHook/src/Components/FormHook.js
+++ Formulario con FormHook/src/Components/FormHook.js
@@ -3,21 +3,24 @@ import { useForm } from "react-hook-form";
 const FormHook = () => {
 
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = () => console.log(errors);
      
     return (
       <form onSubmit={handleSubmit(onSubmit)}>
+
         <label>Name</label>
         <div className={errors.firstName?'border-error':null}>
-            <input {...register("firstName", { required: true, maxLength: 20 })}
-                placeholder='First Name'  />
+            <input {...register("firstName", { required: true, minLength:2, maxLength: 20 })}
+                placeholder='First Name'  
+            />
             {errors.firstName ? <span>X</span> : null}
         </div>
             <p>{errors.firstName?.type === 'required' && "First name is required"}</p>
 
         <label>Last Name</label>
+
         <div className={errors.lastName?'border-error':null}>
-            <input {...register("lastName", {required:true, pattern: /^[A-Za-z]+$/i })}
+            <input {...register("lastName", {required:true, minLength:2, maxLength: 20 })}
                 placeholder='Last Name'
             />
             {errors.lastName ? <span>X</span> : null}
@@ -25,13 +28,13 @@ const FormHook = () => {
         {errors.lastName && <p>Last name is required</p>}
 
         <label>Age</label>
-        <div className={errors.number?'border-error':null}>
-            <input type="number" {...register("age", { min: 18, max: 99 })}
+        <div className={errors.age?'border-error':null}>
+            <input type="number" {...register("age", {required: true, min: 18, max: 99 })}
                 placeholder='Age'
             />
-            {errors.number ? <span>X</span> : null}
+            {errors.age ? <span>X</span> : null}
         </div>
-        <p>{errors.number && 'number must be in range 18 to 99'}</p>
+        <p>{errors.age && 'age must be in range 18 to 99'}</p>
 
         <label>E-mail</label>
         <div className={errors.mail?'border-error':null}>
@@ -43,7 +46,17 @@ const FormHook = () => {
         </div>
         <p>{errors.mail && 'Email must be valid'}</p>
 
+        <label>Password</label>
+        <div className={errors.password?'border-error':null}>
+            <input type='password' {...register("password", { required: true, minLength:2, maxLength: 20 })}
+                placeholder='Password'  
+            />
+            {errors.password ? <span>X</span> : null}
+        </div>
+            <p>{errors.password?.type === 'required' && "First name is required"}</p>
+
         <input type="submit" value='Create User'/>
+        <button onClick={()=>console.log(errors)}>show error</button>
       </form>
     );
 }
